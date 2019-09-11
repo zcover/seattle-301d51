c92e52f4e0dd3e0fa8913eb07b3ea71e6cb3e415
LENA EIVY <applena@gmail.com>
2019-09-10 17:40:50 -0700

    class 2 updated

--- class-02/events-delegation/app.js
+++ class-02/events-delegation/app.js
@@ -2,7 +2,7 @@
 // select all <a> tags that are children of #list
 // when any of them are clicked on, console.log() the text of the <a> tag that was clicked on
 // 'this' refers to the event.target in jQuery
-$( "#list a" ).on( "click", function( event ) {
+$( "#list" ).on( "click", 'a', function( event ) {
   console.log( $( this ).text() );
 });
 
@@ -14,4 +14,7 @@ $( "#list" ).append( "<li><a>I am a dynamically rendered item.</a></li>" );
 $( "#list" ).append( "<li><a>So am I. We don't exist in the HTML</a></li>" );
 
 // how do we make it so that EVERY <a> console.logs() the text?
-// work together - you have 5 minutes to solve this
+// work together - you have 15 minutes to solve this
+  // step 1. dynamically render two new list items just like I did above
+  // step 2. console.log( $(this).text()) whenever anyone clicks on any list item
+  // step 3. make it so that the console log will run even on the dynamically rendered items.
--- class-02/read-JSON-demo/app.js
+++ class-02/read-JSON-demo/app.js
@@ -2,12 +2,51 @@
 
 // GOAL: render each dog and their info to the index page
 
+// "name": "Ginger, Destroyer of Pens",
+// "image_url": "ginger.jpeg",
+// "hobbies": "Eating Cables",
+// "stinky" : "somewhat"
 
+const allDogs = [];
+
+function Dog(dog){
+  this.name = dog.name;
+  this.image_url = dog.image_url;
+  this.hobbies = dog.hobbies;
+
+  allDogs.push(this);
+}
 
 //AJAX 
 
+$.get('/data.json', data => {
+  data.forEach(dog => {
+    var doggie = new Dog(dog);
+    doggie.render();
+  })
+})
   // use ajax to get the data file
   // then run each dog through the constructor function
   // then call a render method on each dog instance
     // we will need an array to hold all the dog instances
 
+Dog.prototype.render = function(){
+  const myTemplate = $('#dog-template').html();
+  const $newSection = $('<section></section>');
+  $newSection.html(myTemplate);
+  
+  $newSection.find('h2').text(this.name);
+  $newSection.find('p').text(this.hobbies);
+  $newSection.find('img').attr('src', this.image_url);
+
+  $('main').append($newSection);
+}
+
+// function renderToPage(){
+//   allDogs.forEach(dog => {
+//     console.log('dog is:', dog);
+//     dog.render();
+//   })
+// }
+
+// renderToPage();
