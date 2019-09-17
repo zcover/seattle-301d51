a6754f360a2cd5e3a63b50344c665dfbd890fb6a
LENA EIVY <applena@gmail.com>
2019-09-16 17:22:53 -0700

    day 6 updated

--- class-06/demo/server.js
+++ class-06/demo/server.js
@@ -1,33 +1,52 @@
 'use strict';
 
-require('dotenv').config();
+// express is the library that lets us set up a server
 const express = require('express');
 const cors = require('cors');
 
+// this initialized our express server
 const app = express();
+
+// tell the browser that it is OK to communicate with the domain that the js isn't running on - tells the browser  that you want the client domain to be able to make server requests.
 app.use(cors());
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`app is listening on port ${PORT}`)
-});
+// dotenv is the library that lets us talk to our .env file
+require('dotenv').config();
 
-app.get('/bananas', (request, response) => {
-  response.send('I am bananas for bananas');
+
+// routes
+app.get('/pizza', (request, response) => {
+  response.send('I like pizza');
 })
 
 app.get('/location', (request, response) => {
-  const tempData = 
-    {
-      "search_query": "seattle",
-      "formatted_query": "Seattle, WA, USA",
-      "latitude": "47.606210",
-      "longitude": "-122.332071"
-    }
-
-    const locationName = request.query.data;
-    console.log(locationName);
-
-
-    response.send(tempData);
+  try{
+    let searchQuery = request.query.data;
+    const geoDataResults = require('./data/geo.json');
+  
+    const locations = new Location(searchQuery, geoDataResults);
+  
+    response.status(200).send(locations);
+  }
+  catch(err){
+    console.error(err);
+  }
 })
+
+// Healper functions
+function Location(searchQuery, geoDataResults){
+  this.searchQuery = searchQuery;
+  this.formatted_query = geoDataResults.results[0].formatted_address;
+  this.latitude = geoDataResults.results[0].geometry.location.lat;
+  this.longitude = geoDataResults.results[0].geometry.location.lng;
+}
+
+
+app.use('*', (request, response) => {
+  response.status(404).send('huh?');
+})
+
+const PORT = process.env.PORT || 3000;
+
+// this starts our express server
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
